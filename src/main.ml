e6793c95223ccc9bfa25d24cff93c0f13c332ebc
huang yi <huangered@hotmail.com>
2016-06-19 20:08:23 +0800

    add simple echo server feature.

--- src/main.ml
+++ src/main.ml
@@ -1,5 +1,9 @@
+open Core.Std
+open Async.Std
 open Server
 let () =
-  send "channel 1" "message";;
-  send "channel 2" "message";;
-  send "channel 3" "message";;
+  send "channel 1" "message";
+  send "channel 2" "message";
+  send "channel 3" "message";
+  run ();
+  never_returns (Scheduler.go())
--- src/server.ml
+++ src/server.ml
@@ -1,3 +1,29 @@
+open Core.Std
+open Async.Std
+
 let send channel message =
   let line = channel ^ ":"  ^ message in
     print_endline line
+
+let rec copy_blocks buffer r w =
+  Reader.read r buffer
+  >>= function
+  |`Eof -> return()
+  |`Ok bytes_read ->
+    print_endline buffer;
+    Writer.write w buffer ~len:bytes_read;
+    Writer.flushed w
+    >>= fun() ->
+    copy_blocks buffer r w
+
+let run ()=
+  print_endline "start server on port: 8765...";
+  let host_and_port = 
+    Tcp.Server.create
+      ~on_handler_error:`Raise
+      (Tcp.on_port 8765)
+      (fun _addr r w ->
+         let buffer = String.create (16 * 1024) in
+         copy_blocks buffer r w)
+   in
+   ignore (host_and_port : (Socket.Address.Inet.t, int) Tcp.Server.t Deferred.t)
