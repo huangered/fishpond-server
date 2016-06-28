6ebb24fc94c7aaf6cdb979080af78a0536a4c9e1
huang yi <huangered@hotmail.com>
2016-06-28 19:46:27 +0800

    Update middleware.ml

--- src/middleware.ml
+++ src/middleware.ml
@@ -1,1 +1,11 @@
 (* middleware.ml *)
+type period = | pre of string
+              | post of string
+              
+let period_to_string = function
+  | pre -> "pre"
+  | post -> "post"
+  
+(* middleware register method *)
+let register_middleware url meth period =
+  print_endline "register middleware"
