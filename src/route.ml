ecd76dd175aac3fa84d3c8b4e73f710a5d3a1fe2
huangered <huangered@icloud.com>
2016-07-08 23:24:21 +0800

    update route

--- src/route.ml
+++ src/route.ml
@@ -1,4 +1,11 @@
 (* route.ml *)
+let register_route_dir url folder =
+  let line =  "register folder: " ^ folder ^ " -> " ^ url in
+    print_endline line
+
+let unregister_route_dir url folder =
+  print_endline "unregister folder"
+
 let register_route url handler =
   print_endline "register route"
   
