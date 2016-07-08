162a11c12dc5b484dffd65c7f99dfba170d1b5ab
huangered <huangered@icloud.com>
2016-07-08 19:07:53 +0800

    1. add resource

--- src/code.ml
+++ src/code.ml
@@ -1,1 +1,9 @@
 (* code.ml the server const variable, http_code, etc *)
+
+type status_code =
+  |100
+  |200
+
+let get_status_code: status_code -> int = function
+  | `Continue -> 100
+  | `Switching_Protocols -> 101
--- src/response.ml
+++ src/response.ml
@@ -1,13 +1,17 @@
-
 open Async.Std
 open Core.Std
 
 type response = 
 {
-	url: string
+  http_ver: string;
+  status_code: string;
+  reason_phrase: string
 }
 
+let status_line resp =
+  resp.http_ver ^ " " ^ resp.status_code ^ " " ^ resp.reason_phrase ^ "\n"
+
 let write_resp buffer r w =
-	Writer.write w buffer ~len:(String.length buffer);
-	Writer.flushed;
-	return ()
+  Writer.write w buffer ~len:(String.length buffer);
+  Writer.flushed;
+  return ()
