00fdbdd300e691b2b8217433e331bcca2b6df3ef
huang yi <huangered@hotmail.com>
2016-07-11 14:55:35 +0800

    Update route.ml

--- src/route.ml
+++ src/route.ml
@@ -1,9 +1,14 @@
 (* route.ml *)
+
+let route_hash = Hashtbl.create 256;;
+
 let register_route_dir url folder =
   let line =  "register folder: " ^ folder ^ " -> " ^ url in
+    Hashtbl.add route_hash url folder;
     print_endline line
 
 let unregister_route_dir url folder =
+  Hashtbl.remove route_hash url;
   print_endline "unregister folder"
 
 let register_route url handler =
