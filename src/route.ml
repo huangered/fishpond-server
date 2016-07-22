8365bea2832702c8556f598ec898eabb243633df
huang yi <huangered@hotmail.com>
2016-07-22 21:47:47 +0800

    add file handler, can write file into response

--- src/route.ml
+++ src/route.ml
@@ -2,14 +2,23 @@
 open Handlersig
 open Request
 open Response
+open String_io
+
+open Sys
 
 let route_hash = Hashtbl.create 256;;
 
 module File_handler = struct
   let name = "file handler"
-  let get req resp  = 
-    {http_ver="HTTP/1.1";status_code=200;reason_phrase="test";message="\ntest"}
-end
+  let get req = 
+    let path = ("."^req.resource) in
+      print_endline ("url:"^path);
+      match Sys.file_exists path && not (Sys.is_directory path) with
+      | false -> {http_ver="HTTP/1.1";status_code=200;reason_phrase="test";message="\nNo found"}
+      | true ->
+               let data = String_io.read_all_file ~f:path in
+                 {http_ver="HTTP/1.1";status_code=200;reason_phrase="test";message=data}
+end 
 
 
 let register_route_dir url folder =
