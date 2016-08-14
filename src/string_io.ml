ecde203ea30e54fce29c20e8633591e07c315289
huang yi <huangered@hotmail.com>
2016-08-14 22:30:55 +0800

    update the read folder method.

--- src/string_io.ml
+++ src/string_io.ml
@@ -3,15 +3,15 @@ open Core.Std
 let read_all_file ~f =
   let lines = In_channel.read_lines f in
     String.concat ~sep:"\r\n" lines
-
-
-let test f =
-  match Sys.is_file f with
-    | `Yes -> print_endline (f^" is file")
-    | `No  -> print_endline (f^" is dir")
+    
+(* the file content cache *)
+let file_hash = String.Table.create() ~size:256;;
 
 (* read all file in the folder *)
 let rec read_fold ~f = 
-  let route_hash = String.Table.create () ~size:256 in
-    let files = Sys.ls_dir f in 
-      List.iter files ~f:test
+  let files = Sys.ls_dir f in 
+     List.iter files ~f:(fun x ->
+       match Sys.is_file (f^"/"^x) with 
+         | `Yes -> Hashtbl.add file_hash ~key:(f^"/"^x) ~data:(f^"/"^x); ()
+         | `No  -> read_fold ~f:(f^"/"^x)
+         | `Unknown -> print_endline ("unknown:"^x))
